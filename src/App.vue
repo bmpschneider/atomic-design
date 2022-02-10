e8055a48321bdb8ecd894ba27303b6752aac86a2
bmpschneider <brunopschneider@gmail.com>
2022-02-09 22:20:03 -0300

    feat: add organisms

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="component">
-    <Service
-      title="Front End"
-      description="here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. "
-    />
+    <Services />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Service } from '@/components/molecules'
+import { Services } from '@/components/organisms'
 
 export default defineComponent({
-  components: { Service }
+  components: { Services }
 })
 </script>
 
