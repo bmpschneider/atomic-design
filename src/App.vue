bd197f0b82a5174a23eb9d82cb05d638987349ad
bmpschneider <brunopschneider@gmail.com>
2022-02-09 21:20:23 -0300

    feat: add molecule services

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="component">
-    <SocialNetworks />
+    <Service
+      title="Front End"
+      description="here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. "
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { SocialNetworks } from '@/components/molecules'
+import { Service } from '@/components/molecules'
 
 export default defineComponent({
-  components: { SocialNetworks }
+  components: { Service }
 })
 </script>
 
